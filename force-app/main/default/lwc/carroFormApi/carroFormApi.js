03c3cc7f77b0353971473b453d8be5d25f41b2d2
BBMiranda <bruno.b.miranda@hotmail.com.br>
2023-06-03 15:54:47 -0300

    Material de Estudos APEX e LWC

--- force-app/main/default/lwc/carroFormApi/carroFormApi.js
+++ force-app/main/default/lwc/carroFormApi/carroFormApi.js
@@ -9,9 +9,9 @@ import MODEL_FIELD from '@salesforce/schema/Carro__c.Modelo__c';
 import TYPE_FIELD from '@salesforce/schema/Carro__c.TipoPintura__c'
 import COLOR_FIELD from '@salesforce/schema/Carro__c.Cor__c';
 
-
 export default class CarroAPIExample extends LightningElement {
     
+  // Variáveis rastreadas para armazenar o estado dos campos e opções selecionados
   @track selectedName = '';
   @track modelOptions = [];
   @track carOptions = [];
@@ -34,91 +34,102 @@ export default class CarroAPIExample extends LightningElement {
       });
   }
 
+  // Função para obter opções de seleção a partir dos dados recebidos
   getOptionsFromData(data) {
       return Object.keys(data).map(key => {
           return { label: key, value: key };
       });
   }
 
+  // Manipulador de evento para alteração do modelo selecionado
   handleModelChange(event) {
     this.selectedModel = event.target.value;
     this.selectedCar = '';
 
-      if (this.selectedModel) {
-        const endpoint = `modelos/${this.selectedModel}`;
-        calloutAPI({ endpoint })
-          .then(result => {
-              this.carOptions = this.getOptionsFromData(result);
-              console.log(result);
-          })
-          .catch(error => {
-              console.error(error);
-          });
-      } else {
-        this.carOptions = [];
-      }
+    if (this.selectedModel) {
+      const endpoint = `modelos/${this.selectedModel}`;
+      calloutAPI({ endpoint })
+        .then(result => {
+            this.carOptions = this.getOptionsFromData(result);
+            console.log(result);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    } else {
+      this.carOptions = [];
+    }
   }
+
+  // Manipulador de evento para alteração do nome do cliente
   handleNameChange(event) {
     this.selectedName = event.target.value;
   }
-  handleCarChange(event) {
-      this.selectedCar = event.target.value;
-      this.selectedPainting = '';
 
-      if (this.selectedCar) {
-        const endpoint = `modelos/esportivo/${this.selectedCar}/cores`;
-        calloutAPI({ endpoint })
-          .then(result => {
-              this.paintingOptions = this.getOptionsFromData(result);
-              console.log(result);
-          })
-          .catch(error => {
-              console.error(error);
-          });
-      } else {
-        this.paintingOptions = [];
-      }
+  // Manipulador de evento para alteração do carro selecionado
+  handleCarChange(event) {
+    this.selectedCar = event.target.value;
+    this.selectedPainting = '';
+
+    if (this.selectedCar) {
+      const endpoint = `modelos/esportivo/${this.selectedCar}/cores`;
+      calloutAPI({ endpoint })
+        .then(result => {
+            this.paintingOptions = this.getOptionsFromData(result);
+            console.log('Testando o Resultado: ' , result);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    } else {
+      this.paintingOptions = [];
+    }
   } 
+
+  // Manipulador de evento para alteração do tipo de pintura selecionado
   handlePaintingChange(event) {
     this.selectedPainting = event.target.value;
   }
+
+  // Manipulador de evento para alteração da cor selecionada
   handleColorChange(event) {
     this.selectedColor = event.target.value;
   }
 
-  // Evento do botão Salvar do formulario
+  // Manipulador de evento para o botão Salvar do formulário
   handleSubmit(event) {
-  const fields = {};
-  fields[NAME_FIELD.fieldApiName] = this.selectedName;
-  fields[MODEL_FIELD.fieldApiName] = this.selectedModel;
-  fields[CAR_FIELD.fieldApiName] = this.selectedCar;
-  fields[TYPE_FIELD.fieldApiName] = this.selectedPainting;
-  fields[COLOR_FIELD.fieldApiName] = this.selectedColor;
+    const fields = {};
+    fields[NAME_FIELD.fieldApiName] = this.selectedName;
+    fields[MODEL_FIELD.fieldApiName] = this.selectedModel;
+    fields[CAR_FIELD.fieldApiName] = this.selectedCar;
+    fields[TYPE_FIELD.fieldApiName] = this.selectedPainting;
+    fields[COLOR_FIELD.fieldApiName] = this.selectedColor;
 
-  const recordInput = { apiName: CAR_OBJECT.objectApiName, fields };
-  createRecord(recordInput)
-      .then(result => {
-          // Lógica de tratamento após salvar com sucesso
-          console.log('Registro salvo com sucesso!');
-          this.carName = '';
-      })
-      .catch(error => {
-          // Lógica de tratamento de erro
-          console.error(error);
-      })
-      this.showToast();
+    const recordInput = { apiName: CAR_OBJECT.objectApiName, fields };
+    createRecord(recordInput)
+        .then(result => {
+            // Lógica de tratamento após salvar com sucesso
+            console.log('Registro salvo com sucesso!');
+            this.carName = '';
+        })
+        .catch(error => {
+            // Lógica de tratamento de erro
+            console.error(error);
+        })
+    this.showToast();
   }
 
-  // Evento Toast
+  // Manipulador de evento para exibir o toast
   showToast(){
     const event = new ShowToastEvent({
-      title: 'Formulário Carro',
+      title: 'Formulário Carro',
       message: 'Salvo com Sucesso',
       variant: 'success',
     })
     this.dispatchEvent(event);
   }
-  // Evento do botão Limpar formulário
+
+  // Manipulador de evento para o botão Limpar formulário
   handleClear(event) {        
     this.selectedName = '';
     this.selectedModel = '';
