6a6cb867846fe153e0aaa9e175732e63e10835ac
BBMiranda <bruno.b.miranda@hotmail.com.br>
2023-06-03 18:35:07 -0300

    Material de Estudos APEX e LWC

--- force-app/main/default/lwc/carroFormApi/carroFormApi.js
+++ force-app/main/default/lwc/carroFormApi/carroFormApi.js
@@ -1,7 +1,7 @@
 import { LightningElement, track } from 'lwc';
-import calloutAPI from '@salesforce/apex/CarroAPIController.calloutAPI';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { createRecord } from 'lightning/uiRecordApi';
+import calloutAPI from '@salesforce/apex/CarroAPIController.calloutAPI';
 import CAR_OBJECT from '@salesforce/schema/Carro__c';
 import NAME_FIELD from '@salesforce/schema/Carro__c.Name';
 import CAR_FIELD from '@salesforce/schema/Carro__c.Carro__c';
@@ -10,21 +10,19 @@ import TYPE_FIELD from '@salesforce/schema/Carro__c.TipoPintura__c'
 import COLOR_FIELD from '@salesforce/schema/Carro__c.Cor__c';
 
 export default class CarroAPIExample extends LightningElement {
-    
-  // Variáveis rastreadas para armazenar o estado dos campos e opções selecionados
+  // Variáveis de acompanhamento (track) para reatividade
   @track selectedName = '';
   @track modelOptions = [];
   @track carOptions = [];
   @track paintingOptions = [];
   @track colorOptions = [];
-  
   @track selectedModel = '';
   @track selectedCar = '';
   @track selectedPainting = '';
   @track selectedColor = '';
-    
+
   connectedCallback() {
-    // Chamada à função do Apex para obter os dados da API
+    // Chamada inicial à API para obter opções de modelo de carro
     calloutAPI({ endpoint: 'modelos' })
       .then(result => {
           this.modelOptions = this.getOptionsFromData(result);
@@ -34,20 +32,22 @@ export default class CarroAPIExample extends LightningElement {
       });
   }
 
-  // Função para obter opções de seleção a partir dos dados recebidos
+  // Método utilitário para converter dados em opções do combobox
   getOptionsFromData(data) {
       return Object.keys(data).map(key => {
           return { label: key, value: key };
       });
   }
 
-  // Manipulador de evento para alteração do modelo selecionado
+  // Manipulador de evento para alteração do modelo de carro selecionado
   handleModelChange(event) {
     this.selectedModel = event.target.value;
     this.selectedCar = '';
 
     if (this.selectedModel) {
       const endpoint = `modelos/${this.selectedModel}`;
+
+      // Chamada à API para obter opções de carros com base no modelo selecionado
       calloutAPI({ endpoint })
         .then(result => {
             this.carOptions = this.getOptionsFromData(result);
@@ -73,10 +73,12 @@ export default class CarroAPIExample extends LightningElement {
 
     if (this.selectedCar) {
       const endpoint = `modelos/esportivo/${this.selectedCar}/cores`;
+
+      // Chamada à API para obter opções de pintura com base no carro selecionado
       calloutAPI({ endpoint })
         .then(result => {
             this.paintingOptions = this.getOptionsFromData(result);
-            console.log('Testando o Resultado: ' , result);
+            console.log(result);
         })
         .catch(error => {
             console.error(error);
@@ -96,7 +98,7 @@ export default class CarroAPIExample extends LightningElement {
     this.selectedColor = event.target.value;
   }
 
-  // Manipulador de evento para o botão Salvar do formulário
+  // Manipulador de evento para envio do formulário
   handleSubmit(event) {
     const fields = {};
     fields[NAME_FIELD.fieldApiName] = this.selectedName;
@@ -106,30 +108,32 @@ export default class CarroAPIExample extends LightningElement {
     fields[COLOR_FIELD.fieldApiName] = this.selectedColor;
 
     const recordInput = { apiName: CAR_OBJECT.objectApiName, fields };
+
+    // Criação de registro usando o serviço createRecord do Lightning Data Service
     createRecord(recordInput)
-        .then(result => {
-            // Lógica de tratamento após salvar com sucesso
-            console.log('Registro salvo com sucesso!');
-            this.carName = '';
-        })
-        .catch(error => {
-            // Lógica de tratamento de erro
-            console.error(error);
-        })
+      .then(result => {
+          console.log('Registro salvo com sucesso!');
+          this.carName = '';
+      })
+      .catch(error => {
+          console.error(error);
+      });
+
     this.showToast();
   }
 
-  // Manipulador de evento para exibir o toast
+  // Exibição de toast de sucesso
   showToast(){
     const event = new ShowToastEvent({
       title: 'Formulário Carro',
       message: 'Salvo com Sucesso',
       variant: 'success',
-    })
+    });
+
     this.dispatchEvent(event);
   }
 
-  // Manipulador de evento para o botão Limpar formulário
+  // Manipulador de evento para limpar o formulário
   handleClear(event) {        
     this.selectedName = '';
     this.selectedModel = '';
